a2e79adcd511fb5f51febc38fef6888c5470a12b
mamut1945 <jamilasep3@gmail.com>
2024-10-20 17:31:03 +0700

    push nih
    ga ga ga ganjar

--- resources/views/components/halaman_utama.blade.php
+++ resources/views/components/halaman_utama.blade.php
@@ -1,16 +1,16 @@
 <div class=" overflow-hidden w-full relative capitalize text-slate-600 ">
     <div class=" relative w-full md:h-screen">
         <div class="w-[1500px] -z-40 h-[1900px] bottom-0 absolute right-0 bg-[#3AD354] bg-opacity-20 rotate-[65deg] translate-y-[1270px] translate-x-[300px] "></div>
-        <div class=" w-full p-10 flex items-center justify-between my-[80px]">
+        <div class=" w-full p-10 flex flex-col sm:flex-row items-center gap-5 justify-between my-[80px]">
             <h1 class="text-2xl">selamat datang $siswa</h1>
-            <div class="flex items-center rounded-md w-[100px] h-[30px] justify-center text-white bg-[#3AD354]">
+            <div class="flex items-center rounded-md w-[100px] h-[30px] justify-center text-white bg-[#30ae45]">
                 <p>tutorial</p>
             </div>
         </div>
-        <div class=" flex-col md:flex-row flex items-center justify-center gap-48 container m-auto">
+        <div class=" flex-col md:flex-row flex items-center justify-center gap-36 sm:gap-20 lg:gap-32 container m-auto">
             <div class="w-[300px] h-[250px] rounded-lg flex p-5 flex-col justify-center shadow-lg hover:shadow-md">
                 <p>tagihan</p>
-                <div class="w-[120px] h-[100px] bg-[#3AD354] flex items-center justify-center  mt-3 mb-6 rounded-md">
+                <div class="w-[120px] h-[100px] bg-[#30ae45] flex items-center justify-center  mt-3 mb-6 rounded-md">
                     <i class='bx bx-wallet text-white text-[50px]'></i>
                 </div>
                 <div class="flex items-center">
@@ -20,7 +20,7 @@
             </div>
             <div class="w-[300px] h-[250px] rounded-lg flex p-5 flex-col justify-center shadow-lg hover:shadow-md">
                 <p>tagihan</p>
-                <div class="w-[120px] h-[100px] bg-[#3AD354] flex items-center justify-center  mt-3 mb-6 rounded-md ">
+                <div class="w-[120px] h-[100px] bg-[#30ae45] flex items-center justify-center  mt-3 mb-6 rounded-md ">
                     <i class='bx bx-money text-white text-[50px]'></i>
                 </div>
                 <div class="flex items-center">
@@ -30,7 +30,7 @@
             </div>
             <div class="w-[300px] h-[250px] rounded-lg flex p-5 flex-col justify-center shadow-lg hover:shadow-md">
                 <p>tagihan</p>
-                <div class="w-[120px] h-[100px] bg-[#3AD354] flex items-center justify-center  mt-3 mb-6 rounded-md">
+                <div class="w-[120px] h-[100px] bg-[#30ae45] flex items-center justify-center  mt-3 mb-6 rounded-md">
                     <i class='bx bx-history text-white text-[50px] '></i>
                 </div>
                 <div class="flex items-center">
--- resources/views/components/navigasi.blade.php
+++ resources/views/components/navigasi.blade.php
@@ -1,44 +1,77 @@
-<div class="flex items-center justify-between w-full py-5 px-7 md:px-16 fixed z-50" x-data="{open: false}">
-    <div class="flex items-center justify-around">
-        <div class="w-[50px] h-[50px] bg-[#3AD354]"></div>
-        <p class="ml-5">smk amaliah 1&2 ciawi</p>
-    </div>
-    <div class="flex items-center relative justify-around">
-        <i class='bx bx-bell text-3xl mr-5 text-slate-500 hover:animate-wiggle cursor-pointer'></i>
-        <i @click="open = !open" class='bx bx-menu text-3xl text-slate-500 cursor-pointer'></i>
-
-        <div x-show="open" x-transition.origin.top.right class="w-[300px] flex p-5 rounded-md  h-[400px] bg-[#3AD354] text-slate-100 shadow-xl absolute right-[-22px] top-[-21.5px]  ">
-            <div class=" w-[250px] h-full  ">
-                <div class="flex">
-                    <div class="w-[50px] h-[50px] mr-2 border-2 flex items-center justify-center rounded-full"><i class="bx bx-user text-3xl"></i></div>
-                    <p class="mt-2">$siswa</p>
+<div class="flex items-center justify-between flex-col w-full z-50" x-data="{open: false}">
+    <div x-show="open" x-transition.origin.bottom.right class="w-full flex p-5 justify-between md:hidden  h-[400px] bg-[#309f42] text-slate-100 shadow-xl  ">
+        <div class=" w-[250px] h-full  ">
+            <div class="flex">
+                <div class="w-[50px] h-[50px] mr-2 border-2 flex items-center justify-center rounded-full"><i class="bx bx-user text-3xl"></i></div>
+                <p class="mt-2">$siswa</p>
+            </div>
+            <div class="flex  flex-col ml-[10px] mt-5 gap-4">
+                <div class="flex items-center gap-2 text-center">
+                    <i class="bx bx-user text-3xl"></i>
+                    <a href="">profil</a>
                 </div>
-                <div class="flex  flex-col ml-[10px] mt-5 gap-4">
-                    <div class="flex items-center gap-2 text-center">
-                        <i class="bx bx-user text-3xl"></i>
-                        <a href="">profil</a>
-                    </div>
-                    <div class="flex items-center gap-2 text-center">
-                        <i class="bx bx-wallet text-3xl"></i>
-                        <a href="">tagihan</a>
-                    </div>
-                    <div class="flex items-center gap-2 text-center">
-                        <i class="bx bx-money text-3xl"></i>
-                        <a href="">pembayaran</a>
-                    </div>
-                    <div class="flex items-center gap-2 text-center">
-                        <i class="bx bx-history text-3xl"></i>
-                        <a href="">riwayat pembayaran</a>
-                    </div>
-                    <div class="flex items-center gap-2 text-center">
-                        <i class="bx bx-headphone text-3xl"></i>
-                        <a href="">hubungi kami</a>
-                    </div>
+                <div class="flex items-center gap-2 text-center">
+                    <i class="bx bx-wallet text-3xl"></i>
+                    <a href="">tagihan</a>
+                </div>
+                <div class="flex items-center gap-2 text-center">
+                    <i class="bx bx-money text-3xl"></i>
+                    <a href="">pembayaran</a>
+                </div>
+                <div class="flex items-center gap-2 text-center">
+                    <i class="bx bx-history text-3xl"></i>
+                    <a href="">riwayat pembayaran</a>
+                </div>
+                <div class="flex items-center gap-2 text-center">
+                    <i class="bx bx-headphone text-3xl"></i>
+                    <a href="">hubungi kami</a>
                 </div>
             </div>
-
-            <i @click="open = !open" class='bx bx-x text-3xl cursor-pointer'></i>
         </div>
 
+        <i @click="open = !open" class='bx bx-x text-3xl cursor-pointer'></i>
+    </div>
+    <div x-show="open" @click="open = !open"  x-transition.origin.top.right class=" w-full hidden md:block h-screen bg-[#30ae45 bg-opacity-20 backdrop-blur-sm fixed right-0 z-10">
+        <div @click.stop  class="flex float-right   w-[400px] h-full bg-[#30ae45] flex-col p-5 text-slate-200   right-0">
+            <div class=" text-xl p-5 ">
+                <div class=" text-center">
+                    <div class="w-[150px] h-[150px] bg-purple-200 rounded-full m-auto mb-5"></div>
+                    <p>$siswa</p>
+                </div>
+
+                <div class="flex items-center gap-2 text-center mt-16 hover:text-slate-300">
+                    <i class="bx bx-user text-3xl "></i>
+                    <a href="">profil</a>
+                </div>
+                <div class="flex items-center gap-2 text-center mt-5 hover:text-slate-300">
+                    <i class="bx bx-wallet text-3xl "></i>
+                    <a href="">tagihan</a>
+                </div>
+                <div class="flex items-center gap-2 text-center mt-5 hover:text-slate-300">
+                    <i class="bx bx-money text-3xl"></i>
+                    <a href="">pembayaran</a>
+                </div>
+                <div class="flex items-center gap-2 text-center mt-5 hover:text-slate-300">
+                    <i class="bx bx-history text-3xl"></i>
+                    <a href="">riwayat pembayaran</a>
+                </div>
+                <div class="flex items-center gap-2 text-center mt-5 hover:text-slate-300">
+                    <i class="bx bx-headphone text-3xl"></i>
+                    <a href="">hubungi kami</a>
+                </div>
+            </div>
+            <i @click="open = !open" class='bx bx-x text-3xl cursor-pointer absolute right-0 top-0 '></i>
+        </div>
+    </div>
+    <div class=" flex justify-between w-11/12 mx-5 mt-5">
+        <div class="flex items-center justify-around">
+            <div class="w-[50px] h-[50px] bg-[#30ae45]"></div>
+            <p class="ml-5">smk amaliah 1&2 ciawi</p>
+        </div>
+        <div class="flex items-center relative justify-around"> 
+            <i class='bx bx-bell text-3xl mr-5 text-slate-500 hover:animate-wiggle cursor-pointer'></i>
+            <i @click="open = !open" class='bx bx-menu text-3xl text-slate-500 cursor-pointer'></i>
+    
+        </div>
     </div>
 </div>

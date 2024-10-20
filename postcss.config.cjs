fdd1f072087bd0589ee20f7a184576999d7cae39
Azka Maulidina <156559528+zMa17@users.noreply.github.com>
2024-10-20 17:39:50 +0700

    koma

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-=======
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
->>>>>>> 070eba8619d1727f64afb0877b29561c2aa05078
 }
